f8a9bbe5b9bf1449e97e40b7275ce4d6d5a2ce68
fitzgerald kachi <57835051+geraldkachi@users.noreply.github.com>
2020-12-30 18:47:35 +0100

    Update FloatBtn.js

--- FloatBtn.js
+++ FloatBtn.js
@@ -28,4 +28,37 @@ const myFloat = {
   align-items: center;
 }
 
+// react- router 
+// 	  from this state 
+//     render() {
+
+//     const { sections } = this.state;
+
+//     const DirectoryArray = sections.length ? (
+//       sections.map(({ id, ...sectionProps }) => (
+//         <MenuItem key={id} {...sectionProps} />
+//       ))
+//     ) : (
+//       <div>No Items...</div>
+//     );
+//     return <div className="directory-menu">{DirectoryArray}</div>;
+//   }
+// }
+////////////////// to this
+	  
+// const MenuItem = ({title, size, imageUrl, history, match, linkUrl}) => {
+//     return (
+//         <div className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
+//             <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}>
+//                 <div className="content">
+//                     <h1 className="title">{title.toUpperCase}</h1>
+//                     <span className="subtitle">SHOP NOW</span>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
 export default FloatBtn
